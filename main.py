a85155f0c313899b347cfecac066e4d753deb20f
Antonio Tsai <jimmyjack7777@gmail.com>
2016-05-21 01:31:03 +0800

    Add totp function

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import hashlib
 import hmac
+import time
 
 
-# HOTP function
-#   Digestmod decide the hash function to perform HMAC.
+# hotp function
+#   hash_mod decide the hash function to perform HMAC.
 #   Option are hashlib.sha1, hashlib.sha224, hashlib.sha256, hashlib.sha384, hashlib.sha512
-def hotp(key, msg, digest_mod):
-    key = key.encode('ascii')
-    msg = msg.encode('ascii')
-    hs = hmac.new(key, msg, digest_mod).hexdigest()
-    # hs = '1f8698690e02ca16618550ef7f19da8e945b555a'
-    print(hs)
-    print(int(hs[-1], 16))
+def hotp(key, msg, hash_mod, length=10):
+    key = str(key).encode('ascii')
+    msg = str(msg).encode('ascii')
+    hs = hmac.new(key, msg, hash_mod).hexdigest()
     offset = int(hs[-1], 16) & 0xF
     p = hs[offset * 2:(offset + 4) * 2]
-    print(p)
-    print(int(p, 16))
-    print(int(p, 16) & 0x7FFFFFFF)
 
-keys = '1234567890'
-message = 'Hello Word!'
-hotp(keys, message, hashlib.sha512)
+    return (int(p, 16) & 0x7FFFFFFF) % 10 ** length
+
+
+def totp(key, time_step=30, t0=0, hash_mod=hashlib.sha512, length=10):
+    t = int((time.time() - t0) / time_step)
+
+    return str(hotp(key, t, hash_mod, length)).rjust(length, '0')
+
+keys = input('key:\n')
+totp_value = totp(keys)
+
+print(totp_value)
